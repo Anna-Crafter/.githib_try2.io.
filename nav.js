3a653e14cac86f26ffb4640449bfae8fd5f6cea4
Anna-Crafter <82431505+Anna-Crafter@users.noreply.github.com>
2021-04-20 12:17:14 +1000

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,16 @@
+<script> {
+    function Hello{} {
+        alert('Hello World!');
+    }
+
+    function openMenu() {
+        var x = document.getElementById("portfolioTopnav");
+       if (x.className === "topnav") {
+        x.className += "responsive";
+       } else {
+           x.className = "topnav";
+       }
+    }
+
+}
+</script>
