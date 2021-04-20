d71ea835d2eb08ba04e89df7f65a4892574cc776
Anna-Crafter <82431505+Anna-Crafter@users.noreply.github.com>
2021-04-20 21:18:44 +1000

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,16 +1,23 @@
-<script> {
-    function Hello{} {
-        alert('Hello World!');
+//creates an alert
+
+alert('Hello World!')
+
+ 
+
+/* Toggle between adding and removing the "responsive" class to topnav  */
+
+function openMenu() {
+
+    var x = document.getElementById("portfolioTopnav");
+
+    if (x.className === "topnav") {
+
+      x.className += "responsive";
+
+    } else {
+
+      x.className = "topnav";
+
     }
 
-    function openMenu() {
-        var x = document.getElementById("portfolioTopnav");
-       if (x.className === "topnav") {
-        x.className += "responsive";
-       } else {
-           x.className = "topnav";
-       }
-    }
-
-}
-</script>
+  }
